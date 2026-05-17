eddc6d2db104adca76f2073b83a3379ceec91852
Jacob Navaratne <jacob.navaratne@outlook.com>
2026-05-17 15:29:13 +0000

    qwerqwerqwerqwet23oi4tvn53290758978752887955555

---
+++ config.js
@@ -0,0 +1,12 @@
+window.APP_CONFIG = {
+  passcode: "0000",
+  firebase: {
+    apiKey: "AIzaSyDrBKFVbRjCNktCmjrv-bI5OSSSj41T4iI",
+    authDomain: "navaratne-books.firebaseapp.com",
+    databaseURL: "https://navaratne-books-default-rtdb.firebaseio.com",
+    projectId: "navaratne-books",
+    storageBucket: "navaratne-books.firebasestorage.app",
+    messagingSenderId: "820433869013",
+    appId: "1:820433869013:web:71d1d2951b7384b4b4aa5c",
+  },
+};
